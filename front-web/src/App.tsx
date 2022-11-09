621033a0c2c8d2df2752eef2a36238914c47611f
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-11-09 19:37:05 -0300

    Integrate Filter with a Pie Chart Card

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -6,10 +6,41 @@ import PieChartCard from './components/pie-chart-card';
 import SalesByDate from './components/sales-by-date';
 import SalesSummary from './components/sales-summary';
 import SalesTable from './components/sales-table';
-import { FilterData } from './types';
+import { FilterData, PieChartConfig, SalesByPaymentMethod, SalesByStore } from './types';
+import { buildFilterParams, makeRequest } from './utils/request';
+import { useEffect, useMemo } from 'react';
+import { buildSalesByPaymentMethod, buildSalesByStoreChart } from './helpers';
 
 function App() {
   const [filterData, setFilterData] = useState<FilterData>();
+  const [salesByStore, setSalesByStore] = useState<PieChartConfig>();
+  const [salesByPaymentMethod, setSalesByPaymentMethod] = useState<PieChartConfig>();
+
+  const params = useMemo(() => buildFilterParams(filterData), [filterData]);
+
+  useEffect(() => {
+    makeRequest
+      .get<SalesByStore[]>('/sales/by-store', { params })
+      .then((response) => {
+        const newSalesByStore = buildSalesByStoreChart(response.data);
+        setSalesByStore(newSalesByStore);
+      })
+      .catch(() => {
+        console.error('Error to fetch sales by date');
+      });
+  }, [params]);
+
+  useEffect(() => {
+    makeRequest
+      .get<SalesByPaymentMethod[]>('/sales/by-payment-method', { params })
+      .then((response) => {
+        const newSalesByPaymentMethod = buildSalesByPaymentMethod(response.data);
+        setSalesByPaymentMethod(newSalesByPaymentMethod);
+      })
+      .catch(() => {
+        console.error('Error to fetch sales by date');
+      });
+  }, [params]);
 
   const onFilterChange = (filter: FilterData) => {
     setFilterData(filter);
@@ -23,15 +54,11 @@ function App() {
         <SalesByDate filterData={filterData} />
         <div className="sales-overview-container">
           <SalesSummary filterData={filterData} />
-          <PieChartCard
-            name="Lojas"
-            labels={['Jaguapitã', 'Londrina', 'Maringá']}
-            series={[40, 30, 30]}
-          />
+          <PieChartCard name="Lojas" labels={salesByStore?.labels} series={salesByStore?.series} />
           <PieChartCard
             name="Pagamentos"
-            labels={['Crédito', 'Débito', 'Dinheiro']}
-            series={[20, 50, 30]}
+            labels={salesByPaymentMethod?.labels}
+            series={salesByPaymentMethod?.series}
           />
         </div>
         <SalesTable />
--- front-web/src/types.ts
+++ front-web/src/types.ts
@@ -22,3 +22,18 @@ export type SalesSummaryData = {
   avg: number;
   count: number;
 };
+
+export type SalesByStore = {
+  storeName: string;
+  sum: number;
+};
+
+export type SalesByPaymentMethod = {
+  description: string;
+  sum: number;
+};
+
+export type PieChartConfig = {
+  labels: string[];
+  series: number[];
+};
