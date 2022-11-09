9c4e1b2dedac5301395e21be67a81a6813882cf3
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-11-09 14:54:12 -0300

    fix: update backend

--- backend/src/main/java/com/devsuperior/dssales/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dssales/controllers/SaleController.java
@@ -3,13 +3,18 @@ package com.devsuperior.dssales.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dssales.dto.SalesByGenderDTO;
+import com.devsuperior.dssales.dto.SalesByStoreDTO;
+import com.devsuperior.dssales.dto.SalesByDateDTO;
+import com.devsuperior.dssales.dto.SalesByPaymentMethodDTO;
+import com.devsuperior.dssales.dto.SalesDTO;
 import com.devsuperior.dssales.dto.SalesSummaryDTO;
 import com.devsuperior.dssales.services.SaleService;
 
@@ -20,17 +25,49 @@ public class SaleController {
 	@Autowired
 	private SaleService service;
 	
-	@GetMapping(value = "/by-gender")
-	public ResponseEntity<List<SalesByGenderDTO>> salesByGender(
-			@RequestParam(value = "storeId", defaultValue = "0") Long storeId) {
-		List<SalesByGenderDTO> list = service.salesByGender(storeId);
+	@GetMapping
+	public ResponseEntity<Page<SalesDTO>> sales(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+			@RequestParam(value = "gender", defaultValue = "") String gender,
+			Pageable pageable) {
+		Page<SalesDTO> page = service.sales(minDate, maxDate, gender, pageable);
+		return ResponseEntity.ok(page);
+	}
+	
+	@GetMapping(value = "/by-store")
+	public ResponseEntity<List<SalesByStoreDTO>> salesByStore(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+			@RequestParam(value = "gender", defaultValue = "") String gender) {
+		List<SalesByStoreDTO> list = service.salesByStore(minDate, maxDate, gender);
 		return ResponseEntity.ok(list);
 	}
-
+	
+	@GetMapping(value = "/by-payment-method")
+	public ResponseEntity<List<SalesByPaymentMethodDTO>> salesByPaymentMethod(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+			@RequestParam(value = "gender", defaultValue = "") String gender) {
+		List<SalesByPaymentMethodDTO> list = service.salesByPaymentMethod(minDate, maxDate, gender);
+		return ResponseEntity.ok(list);
+	}	
+	
+	@GetMapping(value = "/by-date")
+	public ResponseEntity<List<SalesByDateDTO>> salesByDate(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+			@RequestParam(value = "gender", defaultValue = "") String gender) {
+		List<SalesByDateDTO> list = service.salesByDate(minDate, maxDate, gender);
+		return ResponseEntity.ok(list);
+	}	
+	
 	@GetMapping(value = "/summary")
 	public ResponseEntity<SalesSummaryDTO> salesSummary(
-			@RequestParam(value = "storeId", defaultValue = "0") Long storeId) {
-		SalesSummaryDTO obj = service.salesSummary(storeId);
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+			@RequestParam(value = "gender", defaultValue = "") String gender) {
+		SalesSummaryDTO obj = service.salesSummary(minDate, maxDate, gender);
 		return ResponseEntity.ok(obj);
 	}	
 }
--- backend/src/main/java/com/devsuperior/dssales/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dssales/services/SaleService.java
@@ -1,13 +1,21 @@
 package com.devsuperior.dssales.services;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.dssales.dto.SalesByGenderDTO;
+import com.devsuperior.dssales.dto.SalesByStoreDTO;
+import com.devsuperior.dssales.dto.SalesByDateDTO;
+import com.devsuperior.dssales.dto.SalesByPaymentMethodDTO;
+import com.devsuperior.dssales.dto.SalesDTO;
 import com.devsuperior.dssales.dto.SalesSummaryDTO;
+import com.devsuperior.dssales.entities.Gender;
+import com.devsuperior.dssales.entities.Sale;
 import com.devsuperior.dssales.repositories.SaleRepository;
 
 @Service
@@ -17,12 +25,44 @@ public class SaleService {
 	private SaleRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<SalesByGenderDTO> salesByGender(Long storeId) {
-		return repository.salesByGender(storeId);
+	public Page<SalesDTO> sales(String minDate, String maxDate, String gender, Pageable pageable) {
+		LocalDate min = "".equals(minDate) ? null : LocalDate.parse(minDate);
+		LocalDate max = "".equals(maxDate) ? null : LocalDate.parse(maxDate);
+		Gender genderEnum = "".equals(gender) ? null : Gender.valueOf(gender);
+		Page<Sale> page = repository.searchPage(min, max, genderEnum, pageable);
+		repository.salesWithOtherEntities(page.getContent());
+		return page.map(x -> new SalesDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SalesByStoreDTO> salesByStore(String minDate, String maxDate, String gender) {
+		LocalDate min = "".equals(minDate) ? null : LocalDate.parse(minDate);
+		LocalDate max = "".equals(maxDate) ? null : LocalDate.parse(maxDate);
+		Gender genderEnum = "".equals(gender) ? null : Gender.valueOf(gender);
+		return repository.salesByStore(min, max, genderEnum);
 	}
 
 	@Transactional(readOnly = true)
-	public SalesSummaryDTO salesSummary(Long storeId) {
-		return repository.salesSummary(storeId);
+	public List<SalesByPaymentMethodDTO> salesByPaymentMethod(String minDate, String maxDate, String gender) {
+		LocalDate min = "".equals(minDate) ? null : LocalDate.parse(minDate);
+		LocalDate max = "".equals(maxDate) ? null : LocalDate.parse(maxDate);
+		Gender genderEnum = "".equals(gender) ? null : Gender.valueOf(gender);
+		return repository.salesByPaymentMethod(min, max, genderEnum);
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SalesByDateDTO> salesByDate(String minDate, String maxDate, String gender) {
+		LocalDate min = "".equals(minDate) ? null : LocalDate.parse(minDate);
+		LocalDate max = "".equals(maxDate) ? null : LocalDate.parse(maxDate);
+		Gender genderEnum = "".equals(gender) ? null : Gender.valueOf(gender);
+		return repository.salesByDate(min, max, genderEnum);
+	}
+	
+	@Transactional(readOnly = true)
+	public SalesSummaryDTO salesSummary(String minDate, String maxDate, String gender) {
+		LocalDate min = "".equals(minDate) ? null : LocalDate.parse(minDate);
+		LocalDate max = "".equals(maxDate) ? null : LocalDate.parse(maxDate);
+		Gender genderEnum = "".equals(gender) ? null : Gender.valueOf(gender);
+		return repository.salesSummary(min, max, genderEnum);		
 	}
 }
